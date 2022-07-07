6ed1c48ac99b3e779e49bab52e1bfbb1225b9aa0
Abhishek <abhishek.shr2000@gmail.com>
2022-07-07 14:59:41 +0545

    rename testcase in test_user_api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -88,7 +88,7 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_create_token_bad_credentials(self):
+    def test_create_token_emtpy_password(self):
         """Test returns error if password is blank."""
         payload = {
             'email': 'test@example.com',
