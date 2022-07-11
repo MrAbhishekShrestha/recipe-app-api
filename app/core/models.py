c40167ac09b093e282ff6b0d7eea98ab4acfa73b
Abhishek <abhishek.shr2000@gmail.com>
2022-07-11 11:20:34 +0545

    add recipe model to project and makemigrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 """Database Models."""
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -37,3 +38,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     USERNAME_FIELD = "email"
     objects = UserManager()
+
+
+class Recipe(models.Model):
+    """Recipe Object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    links = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
